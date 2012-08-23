320eaba95f9479d6793a506624f9e6e35c5facab
Charlie Burrows <charlie.burrows@gmail.com>
2012-08-23 21:20:57 +0000

    Basic player movement

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
+import "container/list"
+
 type Level struct {
-	cells [][][]Item
+	items [][]*list.List
+	cells [][]Cell
+//	air   [][]Gas
+}
+
+type Cell interface {
+	Walkable() bool
+}
+type Drawable interface {
+	Character() string
 }
 
 type Player struct {
@@ -9,14 +20,29 @@ type Player struct {
 	items []Item
 }
 
-func (p *Player) Move(to [2]int) {
+func (p *Player) Move(to [2]int, level *Level) {
+	for e := level.items[p.pos[0]][p.pos[1]].Front(); e != nil; e = e.Next() {
+		if e.Value == p {
+			level.items[p.pos[0]][p.pos[1]].Remove(e)
+			break
+		}
+	}
 	p.pos = to
+	level.items[p.pos[0]][p.pos[1]].PushFront(p)
+}
+func (p *Player) Walk(dir [2]int, level *Level) {
+	pos := [2]int{p.pos[0] + dir[0], p.pos[1] + dir[1]}
+	if level.cells[pos[0]][pos[1]].Walkable() {
+		p.Move(pos, level)
+	}
 }
 type Item interface {
 	Activate(*Player)
-	Walkable() bool
-	Character() string
 }
+
+type Wall struct {
+}
+func (w *Wall) Character() string { return "#" }
 
 func main() {
 	
