48a5651afb9db594b3db5751f58d2a9182b8e121
Charlie Burrows <charlie.burrows@gmail.com>
2012-08-26 22:35:05 +0000

    Added logging

--- derelict.go
+++ derelict.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"container/list"
+	"log"
+	"os"
 )
 
+var dlog *log.Logger
 ////////////////////// LEVEL /////////////////////////
 
 type Level struct {
@@ -23,7 +26,23 @@ func (level *Level) Init() {
 		}
 	}
 }
+func (level *Level) outerWall() {
+	for i := 0; i < level.x; i++ {
+		level.cells[i][0] = new(Wall)
+	}
+	for i := 0; i < level.x; i++ {
+		level.cells[i][level.y - 1] = new(Wall)
+	}
+	for j := 0; j < level.y; j++ {
+		level.cells[0][j] = new(Wall)
+	}
+	for j := 0; j < level.y; j++ {
+		level.cells[level.x - 1][j] = new(Wall)
+	}
 
+
+
+}
 type Cell interface {
 	Walkable() bool
 }
@@ -34,24 +53,25 @@ type Drawable interface {
 ////////////////////// PLAYER /////////////////////////
 
 type Player struct {
-	pos [2]int
+	x, y int
 	items []Item
 }
 
-func (p *Player) Move(to [2]int, level *Level) {
+func (p *Player) Move(to_x, to_y int) {
 //	for e := level.items[p.pos[0]][p.pos[1]].Front(); e != nil; e = e.Next() {
 //		if e.Value == p {
 //			level.items[p.pos[0]][p.pos[1]].Remove(e)
 //			break
 //		}
 //	}
-	p.pos = to
+	p.x, p.y = to_x, to_y
 //	level.items[p.pos[0]][p.pos[1]].PushFront(p)
 }
-func (p *Player) Walk(dir [2]int, level *Level) {
-	pos := [2]int{p.pos[0] + dir[0], p.pos[1] + dir[1]}
-	if level.cells[pos[0]][pos[1]].Walkable() {
-		p.Move(pos, level)
+func (p *Player) Walk(dir_x, dir_y int, level *Level) {
+	px, py := p.x + dir_x, p.y + dir_y
+	dlog.Println(px, py)
+	if level.cells[px][py].Walkable() {
+		p.Move(px, py)
 	}
 }
 func (p *Player) Character() int32 { return '@' }
@@ -64,13 +84,22 @@ type Item interface {
 
 type Wall struct {
 }
+func (w *Wall) Walkable() bool { return false }
 func (w *Wall) Character() int32 { return '#' }
 
 func main() {
+	file, err := os.Create("log")
+	if err != nil { log.Fatal(err) }
+	dlog = log.New(file, "DERELICT: ", 0)
+
 	var level Level
 	level.x, level.y = 69, 23
 	level.Init()
-	RunCursesUI(&level)
+	level.outerWall()
+
+	var player Player
+	player.x, player.y = 1,1
+	RunCursesUI(&level, &player)
 }
 
 
