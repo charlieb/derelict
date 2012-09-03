edb1517375b51fe7afa85f9b02ce719a27ae62de
Charlie Burrows <charlie.burrows@gmail.com>
2012-09-03 17:28:15 -0400

    Added menus and wall building

--- derelict.go
+++ derelict.go
@@ -7,15 +7,17 @@ import (
 )
 
 var Dlog *log.Logger
+
 ////////////////////// LEVEL /////////////////////////
 
 type Level struct {
-	x, y int
-	items [][]*list.List
-	cells [][]Cell
-	air   [][]float64
+	x, y      int
+	items     [][]*list.List
+	cells     [][]Cell
+	air       [][]float64
 	airBuffer [][]float64
 }
+
 func (level *Level) Init() {
 	level.items = make([][]*list.List, level.x, level.x)
 	level.cells = make([][]Cell, level.x, level.x)
@@ -36,13 +38,13 @@ func (level *Level) outerWall() {
 		level.cells[i][0] = new(Wall)
 	}
 	for i := 0; i < level.x; i++ {
-		level.cells[i][level.y - 1] = new(Wall)
+		level.cells[i][level.y-1] = new(Wall)
 	}
 	for j := 0; j < level.y; j++ {
 		level.cells[0][j] = new(Wall)
 	}
 	for j := 0; j < level.y; j++ {
-		level.cells[level.x - 1][j] = new(Wall)
+		level.cells[level.x-1][j] = new(Wall)
 	}
 }
 func (level *Level) Iterate() {
@@ -61,7 +63,7 @@ func (level *Level) Iterate() {
 			Dlog.Printf("   Level.Iterate cell: (%v, %v)\n", i, j)
 			for ii := -1; ii <= 1; ii++ {
 				for jj := -1; jj <= 1; jj++ {
-					if i + ii >= 0 && i + ii < level.x && j + jj >= 0 && j + jj < level.y {
+					if i+ii >= 0 && i+ii < level.x && j+jj >= 0 && j+jj < level.y {
 						Dlog.Printf("   Level.Iterate (%v, %v)\n", i+ii, j+jj)
 						if level.cells[i+ii][j+jj] == nil || level.cells[i+ii][j+jj].Walkable() {
 							total += level.air[i+ii][j+jj]
@@ -70,7 +72,9 @@ func (level *Level) Iterate() {
 					}
 				}
 			}
-			if nairs == 0 || total == 0 { continue }
+			if nairs == 0 || total == 0 {
+				continue
+			}
 			Dlog.Printf("   Level.Iterate (%v, %v) airs: %v, total: %v\n", i, j, nairs, total)
 			level.airBuffer[i][j] = total / nairs
 		}
@@ -80,6 +84,7 @@ func (level *Level) Iterate() {
 	level.airBuffer = tmp
 	Dlog.Println("<- Level.Iterate")
 }
+
 type Cell interface {
 	Walkable() bool
 
@@ -100,8 +105,9 @@ type Sensor interface {
 	Sense(level *Level, player *Player) float64
 }
 
-type PressureSensor struct {}
-func (s *PressureSensor) Name() string { return "Pressure Sensor" }
+type PressureSensor struct{}
+
+func (s *PressureSensor) Name() string        { return "Pressure Sensor" }
 func (s *PressureSensor) Description() string { return "Short range ambient air pressure sensor" }
 func (s *PressureSensor) Activate(level *Level, player *Player) {
 	// Add pressure sensor to the suit
@@ -110,34 +116,34 @@ func (s *PressureSensor) Activate(level *Level, player *Player) {
 ////////////////////// SUIT /////////////////////////
 
 type Suit struct {
-	air float64
-	airCapacity float64
+	air           float64
+	airCapacity   float64
 	faceplateOpen bool
 }
 
 ////////////////////// PLAYER /////////////////////////
 
 type Player struct {
-	x, y int
+	x, y  int
 	items []Item
 
 	vision int
 }
 
 func (p *Player) Move(to_x, to_y int) {
-//	for e := level.items[p.pos[0]][p.pos[1]].Front(); e != nil; e = e.Next() {
-//		if e.Value == p {
-//			level.items[p.pos[0]][p.pos[1]].Remove(e)
-//			break
-//		}
-//	}
+	//	for e := level.items[p.pos[0]][p.pos[1]].Front(); e != nil; e = e.Next() {
+	//		if e.Value == p {
+	//			level.items[p.pos[0]][p.pos[1]].Remove(e)
+	//			break
+	//		}
+	//	}
 	Dlog.Println("-> Move", to_x, to_y)
 	p.x, p.y = to_x, to_y
 	Dlog.Println("<- Move", p.x, p.y)
-//	level.items[p.pos[0]][p.pos[1]].PushFront(p)
+	//	level.items[p.pos[0]][p.pos[1]].PushFront(p)
 }
 func (p *Player) Walk(dir_x, dir_y int, level *Level) bool {
-	px, py := p.x + dir_x, p.y + dir_y
+	px, py := p.x+dir_x, p.y+dir_y
 	Dlog.Println("-> Walk", px, py)
 	if px >= 0 && px < level.x && py >= 0 && py < level.y {
 		if level.cells[px][py] == nil || level.cells[px][py].Walkable() {
@@ -150,22 +156,55 @@ func (p *Player) Walk(dir_x, dir_y int, level *Level) bool {
 	return false
 }
 func (p *Player) Character() int32 { return '@' }
+func (p *Player) Action(level *Level, ui UI) bool {
+	Dlog.Println("-> Player.Action")
+	action, abort := ui.Menu([]string{"Activate",
+		"-",
+		"Build Wall",
+		"Repair Wall"})
+		Dlog.Println("   Player.Action: ", action, abort)
+	if abort {
+		Dlog.Println("-> Player.Action: false")
+		return false
+	}
+	switch action {
+	case 0: // Activate
+		x, y, abort := ui.DirectionPrompt()
+		if !abort && level.cells[p.x + x][p.y + y] == nil {
+		}
+	case 1: // Build Wall
+		x, y, abort := ui.DirectionPrompt()
+		if abort {
+			return false
+		} else if level.cells[p.x+x][p.y+y] == nil {
+			level.cells[p.x+x][p.y+y] = new(Wall)
+			ui.Message("Built a wall")
+		} else {
+			ui.Message("That square is occupied")
+		}
+	case 2: // Repair Wall
+		ui.Message("Repair a wall!")
+	}
+	Dlog.Println("<- Player.Action: true")
+	return true
+}
 
 ////////////////////// /////////////////////////
-
 
 type sCell struct {
 	visible bool
 }
+
 func (c *sCell) Walkable() bool { return true }
-func (c *sCell) SeePast() bool { return true }
+func (c *sCell) SeePast() bool  { return true }
 
 type Wall struct {
 	sCell
 }
-func (w *Wall) Walkable() bool { return false }
+
+func (w *Wall) Walkable() bool   { return false }
 func (w *Wall) Character() int32 { return '#' }
-func (w *Wall) SeePast() bool { return false }
+func (w *Wall) SeePast() bool    { return false }
 
 func buildTestWalls(level *Level) {
 	for i := 2; i < 5; i++ {
@@ -175,12 +214,14 @@ func buildTestWalls(level *Level) {
 		level.cells[2][j] = new(Wall)
 	}
 }
+
 /////////////////// ///////////////////
 type Game struct {
-	level Level
+	level  Level
 	player Player
-	ui UI
+	ui     UI
 }
+
 func NewGame() Game {
 	var game Game
 	game.level.x, game.level.y = 69, 23
@@ -191,18 +232,18 @@ func NewGame() Game {
 
 	buildTestWalls(&game.level)
 
-	game.player.x, game.player.y, game.player.vision = 1,1,5
+	game.player.x, game.player.y, game.player.vision = 1, 1, 5
 
 	return game
 }
 func main() {
 	file, err := os.Create("log")
-	if err != nil { log.Fatal(err) }
+	if err != nil {
+		log.Fatal(err)
+	}
 	Dlog = log.New(file, "DERELICT: ", 0)
 
 	game := NewGame()
 	game.ui = new(CursesUI)
 	game.ui.Run(&game)
 }
-
-
